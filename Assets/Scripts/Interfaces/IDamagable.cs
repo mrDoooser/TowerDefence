7c8869aa3f8126e65a8ca90282ceec914b84e39c
mrDoooser <mrdoooser@gmail.com>
2020-02-10 10:35:57 +0200

    Cleaning

--- Assets/Scripts/Interfaces/IDamagable.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public interface IDamagable
-{
-    float Health { get; set; }
-    float MaxHealth { get; set; }
-    void Initialize(float MaxHealth);
-    void TakeDamage(DamageType DamageType, float Damage);
-    void Die();
-}
